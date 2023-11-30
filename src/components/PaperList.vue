52c3476faba0601405572f5c6ddb52b6985454ff
Tong Zhu (朱桐) <tzhu1997@outlook.com>
2023-11-30 13:29:29 +0800

    update cron interval, add last update time, add bottom-to-top navigation, hide selected number under category button click, update category paper num

--- src/components/PaperList.vue
+++ src/components/PaperList.vue
@@ -1,24 +1,29 @@
 <script setup>
-import { computed, ref, watch } from 'vue'
+import { computed, ref, watch, onMounted } from 'vue'
 
 import cat2papers from '../../papers_with_style.json'
 import rawCat2papers from '../../papers.json'
+import settings from '../../settings.json'
 
 // cat2papers: { "cs.CL": [{"title": ""}, ...], "cs.AI": [{"title": "", ...}, ...] }
 const categories = ref(Object.keys(cat2papers))
 const papers = ref([])
 const categoryString = ref("")
-const paperNum = ref(0)
 const isChecked = ref([])
 const showAbs = ref([])
 const paperDates = ref("")
 
+// Compute the number of papers for each category
+const paperNum = ref({})
+Object.keys(cat2papers).forEach((category) => {
+  paperNum.value[category] = cat2papers[category].length
+})
+
 const showPapers = (category) => {
   papers.value = cat2papers[category]
   categoryString.value = category
-  paperNum.value = papers.value.length
-  isChecked.value = Array(paperNum.value).fill(false)
-  showAbs.value = Array(paperNum.value).fill(false)
+  isChecked.value = Array(paperNum.value[category]).fill(false)
+  showAbs.value = Array(paperNum.value[category]).fill(false)
   const uniqueDates = [...new Set(papers.value.map((paper) => paper.date))]
   paperDates.value = uniqueDates.join(', ')
 }
@@ -27,7 +32,7 @@ let allIsSelected = false
 const selectAllButtonString = ref("Select All")
 const selectAll = () => {
   allIsSelected = !allIsSelected
-  isChecked.value = Array(paperNum.value).fill(allIsSelected)
+  isChecked.value = Array(paperNum.value[categoryString.value]).fill(allIsSelected)
   selectAllButtonString.value = allIsSelected ? "Unselect All" : "Select All"
 }
 
@@ -45,27 +50,6 @@ const exportToClipboard = () => {
   noteMsg.value = `Successfully export ${numSelected.value} papers of ${categoryString.value} to clipboard~`
 }
 
-// const exportToClipboard = () => {
-//   const selectedPapers = papers.value.filter((_, index) => isChecked.value[index])
-//   const paperString = selectedPapers.map((paper) => {
-//     // Format the title and URL as HTML
-//     return `<td>${paper.title} <br style="mso-data-placement:same-cell;" /> <a href="${paper.url}">${paper.url}</a></td>`
-//   }).join('</tr><tr>')
-//   console.log(`<table><tr>${paperString}</tr></table>`)
-
-//   const blob = new Blob([`<table><tr>${paperString}</tr></table>`], { type: 'text/html' })
-//   const data = new ClipboardItem({ 'text/html': blob })
-
-//   navigator.clipboard.write([data])
-//     .then(() => {
-//       console.log('Text copied to clipboard')
-//       noteMsg.value = `Successfully export ${numSelected.value} papers of ${categoryString.value} to clipboard~`
-//     })
-//     .catch(err => {
-//       noteMsg.value = `Error in copying text: ${err}`
-//     })
-// }
-
 watch(noteMsg, (newValue) => {
   if (newValue) {
     setTimeout(() => {
@@ -74,19 +58,56 @@ watch(noteMsg, (newValue) => {
   }
 })
 
+const lastUpdate = ref("")
+
+onMounted(getLatestCommitDateTime)
+
+
+async function getLatestCommitDateTime() {
+  try {
+    const apiUrl = `https://api.github.com/repos/${settings.owner}/${settings.repo}/branches/${settings.branch}`
+    const response = await fetch(apiUrl)
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+
+    const branchData = await response.json()
+    const commit = branchData.commit.commit
+    const commitDate = new Date(commit.committer.date)
+    const formattedDate = commitDate.toLocaleString('en-US', { month: 'short', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZone: 'Asia/Shanghai' })
+    lastUpdate.value = formattedDate
+    console.log(`Last update date: ${formattedDate}`)
+  } catch (error) {
+    console.error('Error:', error.message)
+  }
+}
+
+function navigateToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  })
+}
+
 </script>
 
 <template>
+  <!-- Existing code... -->
   <div class="button-group">
     <button @click="exportToClipboard"><i class="fas fa-file-export"></i> Export to Clipboard</button>
     <button @click="selectAll"><i class="fas fa-check-square"></i> {{ selectAllButtonString }}</button>
   </div>
   <p v-if="noteMsg" class="bold">{{ noteMsg }}</p>
   <div class="button-group">
-    <button v-for="category in categories" :key="category" @click="showPapers(category)">{{ category }}</button>
+    <button v-for="category in categories" :key="category" @click="showPapers(category)">{{ category }} ({{
+      paperNum[category] }})</button>
   </div>
-  <p>Papers under {{ categoryString }} : {{ paperNum }}, selected : {{ numSelected }}</p>
-  <p>Paper dates: {{ paperDates }}</p>
+  <p>Last update time: {{ lastUpdate }}</p>
+  <div v-if="categoryString">
+    <p>Selected {{ numSelected }} / {{ paperNum[categoryString] }} from {{ categoryString }}</p>
+    <p>Paper dates: {{ paperDates }}</p>
+  </div>
   <div class="paper-list">
     <div v-for="(paper, index) in papers" :key="paper.url">
       <div class="row title" @click="isChecked[index] = !isChecked[index]">
@@ -94,7 +115,8 @@ watch(noteMsg, (newValue) => {
         <p class="col no-margin" v-html="paper.title"></p>
       </div>
       <p class="no-margin"><span class="bold">Authors: </span><span v-html="paper.authors.join(', ')"></span></p>
-      <p class="no-margin" v-if="paper.comment"><span class="bold">Comment: </span><span v-html="paper.comment"></span></p>
+      <p class="no-margin" v-if="paper.comment"><span class="bold">Comment: </span><span v-html="paper.comment"></span>
+      </p>
       <div class="no-margin">
         <a class="badge badge-link" :href="paper.url" target="_blank">Link</a>
         <a class="badge badge-pdf" :href="paper.pdf_url" target="_blank">PDF</a>
@@ -102,6 +124,12 @@ watch(noteMsg, (newValue) => {
         <p class="text-block" v-if="showAbs[index]" v-html="paper.abstract"></p>
       </div>
     </div>
+  </div>
+  <div>
+    <!-- Floating "Navigate to Top" button -->
+    <button class="navigate-top-button" @click="navigateToTop">
+      <i class="fas fa-arrow-up"></i>
+    </button>
   </div>
 </template>
 
@@ -128,6 +156,27 @@ watch(noteMsg, (newValue) => {
 .emph {
   color: #d05130;
   font-style: italic;
+}
+
+.navigate-top-button {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: #333;
+  color: #fff;
+  font-size: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+}
+
+.navigate-top-button:hover {
+  background-color: #555;
 }
 
 </style>
